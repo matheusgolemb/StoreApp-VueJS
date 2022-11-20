953960265393785712ffc6d5da00031245e1a9e3
matheus santana <mgolem017@gmail.com>
2022-11-19 17:51:15 -0800

    Adding comments to explain what im trying to do

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <template>
   <div>
     <main-menu :loginInfo="loginValid"></main-menu>
+    <!-- Here I also tried to bind the loginValid variable, so then, after the login is successfull i would change the value to true. So the LI in the header would be different after user logged in. Like Milad asked in the coursework steps 2. and 3. -->
     <div class="container-fluid">
       <router-view @getUserData="getLoginData(UserData)"></router-view>
     </div>
@@ -24,6 +25,9 @@ export default {
   methods:{
     getLoginData(UserData){
       console.log(UserData)
+      // Here I am trying to console log the information of the logged user. I tried to send this info by the LoginPage component in the doLogin() function. But, as u can see the console log is returning empty.
+
+
       // this.UserData = UserData;
     }
   }
